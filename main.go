e6d32a828694c4b6d22761d3363649b7e387a2be
John Saigle <nothanks>
2026-02-26 08:00:36 -0500

    add nil pointer deref

--- main.go
+++ main.go
@@ -8,8 +8,16 @@ import (
 )
 
 func main() {
+
+	var i *int
+	add(i)
+
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(2)
 	}
+}
+
+func add(i *int) int {
+	return 1 + *i
 }
